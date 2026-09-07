a6e912896aa814a8a1682ab05c6ea3ece5aedc9a
Haidar <ar9353023@gmail.com>
2026-09-07 15:47:35 +0300

    add

--- lib/feature/ui/logic_screen.dart
+++ lib/feature/ui/logic_screen.dart
@@ -5,6 +5,8 @@ class LogicScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(),
+    );
   }
 }
